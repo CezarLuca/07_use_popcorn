6ea18ff87abebe35407eb0dd8e860dc6c22e3fb1
CezarLuca <71207294+CezarLuca@users.noreply.github.com>
2024-02-23 23:20:22 +0100

    Improving Reusability With Props part 1

--- src/StarRating.js
+++ src/StarRating.js
@@ -12,20 +12,25 @@ const starContainerStyle = {
     alignItems: "center",
 };
 
-const textStyle = {
-    fontSize: "1.5rem",
-    fontWeight: "bold",
-    lineHeight: "1",
-    margin: "0",
-};
-
-export default function StarRating({ maxRating = 5 }) {
+export default function StarRating({
+    maxRating = 5,
+    color = "#fcc419",
+    size = "1.5rem",
+}) {
     const [rating, setRating] = useState(0);
     const [hoverRating, setHoverRating] = useState(0);
 
     function handleRating(rating) {
         setRating(rating);
     }
+
+    const textStyle = {
+        fontSize: `${size}`,
+        fontWeight: "bold",
+        lineHeight: "1",
+        margin: "0",
+        color: color,
+    };
 
     return (
         <div style={containerStyle}>
@@ -39,6 +44,8 @@ export default function StarRating({ maxRating = 5 }) {
                         }
                         onHover={() => setHoverRating(i + 1)}
                         onUnhover={() => setHoverRating(0)}
+                        color={color}
+                        size={size}
                     />
                 ))}
             </div>
@@ -47,14 +54,20 @@ export default function StarRating({ maxRating = 5 }) {
     );
 }
 
-const starStyle = {
-    width: "2rem",
-    height: "2rem",
-    display: "block",
-    cursor: "pointer",
-};
-
-function Star({ onRating, full, onHover, onUnhover }) {
+function Star({
+    onRating,
+    full,
+    onHover,
+    onUnhover,
+    color = "#fcc419",
+    size = "1.5rem",
+}) {
+    const starStyle = {
+        width: `${size}`,
+        height: `${size}`,
+        display: "block",
+        cursor: "pointer",
+    };
     return (
         <span
             role="button"
@@ -67,8 +80,8 @@ function Star({ onRating, full, onHover, onUnhover }) {
                 <svg
                     xmlns="http://www.w3.org/2000/svg"
                     viewBox="0 0 20 20"
-                    fill="#000"
-                    stroke="#000"
+                    fill={color}
+                    stroke={color}
                 >
                     <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
                 </svg>
@@ -77,7 +90,7 @@ function Star({ onRating, full, onHover, onUnhover }) {
                     xmlns="http://www.w3.org/2000/svg"
                     fill="none"
                     viewBox="0 0 24 24"
-                    stroke="#000"
+                    stroke={color}
                 >
                     <path
                         strokeLinecap="round"
