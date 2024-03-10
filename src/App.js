765463e33880b17ab5750a0d85208f13856aab32
CezarLuca <71207294+CezarLuca@users.noreply.github.com>
2024-03-10 21:29:57 +0100

    Adding a loading state done

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
 import { useEffect, useState } from "react";
 import KEY from "./KEY";
 
-const tempMovieData = [
-    {
-        imdbID: "tt1375666",
-        Title: "Inception",
-        Year: "2010",
-        Poster: "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg",
-    },
-    {
-        imdbID: "tt0133093",
-        Title: "The Matrix",
-        Year: "1999",
-        Poster: "https://m.media-amazon.com/images/M/MV5BNzQzOTk3OTAtNDQ0Zi00ZTVkLWI0MTEtMDllZjNkYzNjNTc4L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SX300.jpg",
-    },
-    {
-        imdbID: "tt6751668",
-        Title: "Parasite",
-        Year: "2019",
-        Poster: "https://m.media-amazon.com/images/M/MV5BYWZjMjk3ZTItODQ2ZC00NTY5LWE0ZDYtZTI3MjcwN2Q5NTVkXkEyXkFqcGdeQXVyODk4OTc3MTY@._V1_SX300.jpg",
-    },
-];
+// const tempMovieData = [
+//     {
+//         imdbID: "tt1375666",
+//         Title: "Inception",
+//         Year: "2010",
+//         Poster: "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg",
+//     },
+//     {
+//         imdbID: "tt0133093",
+//         Title: "The Matrix",
+//         Year: "1999",
+//         Poster: "https://m.media-amazon.com/images/M/MV5BNzQzOTk3OTAtNDQ0Zi00ZTVkLWI0MTEtMDllZjNkYzNjNTc4L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SX300.jpg",
+//     },
+//     {
+//         imdbID: "tt6751668",
+//         Title: "Parasite",
+//         Year: "2019",
+//         Poster: "https://m.media-amazon.com/images/M/MV5BYWZjMjk3ZTItODQ2ZC00NTY5LWE0ZDYtZTI3MjcwN2Q5NTVkXkEyXkFqcGdeQXVyODk4OTc3MTY@._V1_SX300.jpg",
+//     },
+// ];
 
 const tempWatchedData = [
     {
@@ -50,6 +50,7 @@ export default function App() {
     const [movies, setMovies] = useState([]);
     const [watched, setWatched] = useState([]);
     const [query, setQuery] = useState("interstellar");
+    const [isLoading, setIsLoading] = useState(false);
 
     // fetch(`https://www.omdbapi.com/?apikey=${KEY}&s=interstellar`)
     //     .then((res) => res.json())
@@ -58,12 +59,14 @@ export default function App() {
 
     useEffect(() => {
         async function fetchMovies() {
+            setIsLoading(true);
             const res = await fetch(
                 `https://www.omdbapi.com/?apikey=${KEY}&s=${query}`
             );
             const data = await res.json();
             setMovies(data.Search);
             console.log(data.Search);
+            setIsLoading(false);
         }
         // fetch(`https://www.omdbapi.com/?apikey=${KEY}&s=interstellar`)
         //     .then((res) => res.json())
@@ -82,7 +85,7 @@ export default function App() {
             </NavBar>
             <Main>
                 <Box>
-                    <MovieList movies={movies} />
+                    {isLoading ? <Loader /> : <MovieList movies={movies} />}
                 </Box>
                 <Box>
                     <Summary watched={watched} />
@@ -91,6 +94,10 @@ export default function App() {
             </Main>
         </>
     );
+}
+
+function Loader() {
+    return <p className="loader">Loading...</p>;
 }
 
 function NavBar({ children }) {
