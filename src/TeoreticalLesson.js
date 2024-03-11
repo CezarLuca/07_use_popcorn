2e08463e87d21b78dd04a09c8222ff5591bb8963
CezarLuca <71207294+CezarLuca@users.noreply.github.com>
2024-03-11 23:55:03 +0100

    Teoretical Lesson part 1

---
+++ src/TeoreticalLesson.js
@@ -0,0 +1,10 @@
+// ---------------------------------------------------------
+/* 9. The useEffect Dependency Array
+
+    - The Dependency Array:
+        - By default, effects run after every render. We can 
+        prevent that by passing a dependency array;
+        - Without the dependency array, React doenst know when
+        to re-run the effect;
+
+*/
