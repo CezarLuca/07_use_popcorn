299318857950facac82062eba1be9874b14eda99
CezarLuca <71207294+CezarLuca@users.noreply.github.com>
2024-03-14 23:46:43 +0100

    Added capitalization to the SearchBar input values

--- src/App.js
+++ src/App.js
@@ -160,13 +160,18 @@ function Logo() {
 }
 
 function SearchBar({ query, setQuery }) {
+    const handleChange = (e) => {
+        const value = e.target.value;
+        const capitalizedValue = value.charAt(0).toUpperCase() + value.slice(1);
+        setQuery(capitalizedValue);
+    };
     return (
         <input
             className="search"
             type="text"
             placeholder="Search movies..."
             value={query}
-            onChange={(e) => setQuery(e.target.value)}
+            onChange={handleChange}
         />
     );
 }
