20094bbc38f4a4dbd371404eee20875df0e99734
CezarLuca <71207294+CezarLuca@users.noreply.github.com>
2024-03-22 22:09:16 +0100

    Teoretical Lesson 2

--- src/extras/TeoreticalLessons.js
+++ src/extras/TeoreticalLessons.js
@@ -1,1 +1,14 @@
+// ----------------------------------------------------------------------------
 // 160 - React Hooks and their Rules
+
+/* 
+    1. React Hooks are special built-in functions that allow 
+    you to use state and other React features in functional
+    components. They allow us to "hook into".
+    React internals:
+        - Creating and accessing state from Fiber tree;
+        - Registring side effects in the Fiber tree;
+        - Manual DOM selections;
+        - etc.
+    2. React Hooks are functions that always start with "use" keyword.
+*/
