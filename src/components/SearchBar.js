24b8190d949e5bf0de3173de1f9d11312a35557a
CezarLuca <71207294+CezarLuca@users.noreply.github.com>
2024-03-25 23:26:48 +0100

    Refs to Select DOM Elements part 1

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,9 +1,13 @@
+import { useRef } from "react";
+
 export default function SearchBar({ query, setQuery }) {
     const handleChange = (e) => {
         const value = e.target.value;
         const capitalizedValue = value.charAt(0).toUpperCase() + value.slice(1);
         setQuery(capitalizedValue);
     };
+
+    const inputElement = useRef(null);
     return (
         <input
             className="search"
@@ -11,6 +15,7 @@ export default function SearchBar({ query, setQuery }) {
             placeholder="Search movies..."
             value={query}
             onChange={handleChange}
+            ref={inputElement}
         />
     );
 }
